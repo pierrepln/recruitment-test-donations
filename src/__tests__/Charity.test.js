bfbd9d0aef5716c5a643cd9688b2ca556df70033
Pierre Plantié <pierrepl@ntie.fr>
2016-12-18 16:58:01 +0000

    add test on charity title

--- src/__tests__/Charity.test.js
+++ src/__tests__/Charity.test.js
@@ -7,4 +7,9 @@ describe('Charity Component', function () {
   it('renders without crashing', () => {
     shallow(<Charity />);
   });
+  it('renders a title', () => {
+    const wrapper = shallow(<Charity />);
+    const title = <h3>Charity</h3>;
+    expect(wrapper.contains(title)).toEqual(true);
+  });
 });
